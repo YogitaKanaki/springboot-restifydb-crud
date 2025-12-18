311044d212348931b131530f95d6f454db3dab20
YogitaKanaki <yogitakanaki20@gmail.com>
2025-12-18 11:16:35 +0530

    Initial commit: Spring Boot CRUD automation using RestifyDB

---
+++ src/test/java/com/example/restifydbproject/RestifydbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restifydbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestifydbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
